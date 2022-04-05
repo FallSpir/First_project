ce963d9254a025d1120dc140d2c5a29c9481ea8f
FallSpir <baodayyt@gmail.com>
2022-04-05 17:12:45 +0300

    Adding github API example😀

--- a1.py
+++ a1.py
@@ -1,1 +1,10 @@
-print('Hello, World!')
+import requests as r
+import json
+
+
+url = 'https://api.github.com/users/FallSpir/repos'
+response = r.get(url)
+response_json = response.json
+
+for repo in response_json:
+    print(f'Repo:{repo["name"]}, URL:{repo["html_url"]}')
